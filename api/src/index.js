bc055343cdaefb6b540814c284c65e1fef482853
ath-gsy <107257470+ath-gsy@users.noreply.github.com>
2022-11-30 14:22:12 +0100

    GET et POST (toujours erreur sur home)

--- api/src/index.js
+++ api/src/index.js
@@ -7,6 +7,10 @@ import bodyParser from 'body-parser';
 
 import { checkJwt } from './auth/jwt-middleware.js'; //
 
+import { postBeep, BeepTooLongError } from './use-case/post-beep.js'; //
+
+import { getUserHome } from './use-case/get-user-home.js';
+
 const app = express();
 
 app.use(cors());
@@ -20,8 +24,26 @@ app.use(checkJwt); //
 //   res.status(200).send('Hello world');
 // });
 
-app.get('/', (req, res) => {
-  res.status(200).send(`Hello ${req.auth.sub}`);
+// app.get('/', (req, res) => {
+//   res.status(200).send(`Hello ${req.auth.sub}`);
+// });
+
+app.get('/home', async (req, res) => {
+  const home = await getUserHome(req.auth.sub);
+  res.status(200).json(home);
+});
+
+app.post('/beep', async (req, res) => {
+  try {
+    const postedBeep = await postBeep(req.auth.sub, req.body.content);
+    res.status(201).json(postedBeep);
+  } catch (e) {
+    if (e instanceof BeepTooLongError) {
+      res.status(400).send('Beep too long');
+    } else {
+      throw e;
+    }
+  }
 });
 
 app.listen(8090);
